f07f5d42d2df0146e78e75781c02a30d265fcef7
mikeh0rn <mikelaurencehorn@gmail.com>
2019-01-13 11:50:01 -0800

    test site for h2h

---
+++ send.php
@@ -0,0 +1,11 @@
+<?php
+$q1=$_POST['q1'];
+$q2=$_POST['q2'];
+$q3=$_POST['q3'];
+$q4=$_POST['q4'];
+$to = "mhorn918@gmail.com";
+$subject = "Hi Mike $q3";
+$message= "$q1 $q2 :\n\n: $q1\n\n: $q2\n\nEmail: $q3\n\nTel: $q4\n";
+mail($to,$subject,$message);
+header("Location:index.php");
+?>
