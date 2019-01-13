1d028523e3dbd9b0563aa0c34eb08a73a56552d7
mikeh0rn <mikelaurencehorn@gmail.com>
2019-01-13 12:06:50 -0800

    update send and index

--- send.php
+++ send.php
@@ -1,11 +1,13 @@
-<?
+<?php
+
 $q1=$_POST['q1'];
 $q2=$_POST['q2'];
 $q3=$_POST['q3'];
 $q4=$_POST['q4'];
-$to = "mhorn918@gmail.com";
-$subject = "Hi Mike $q3";
-$message= "$q1 $q2 :\n\n: $q1\n\n: $q2\n\nEmail: $q3\n\nTel: $q4\n";
-mail($to,$subject,$message);
-header("Location:index.php");
+
+$to_email = 'mhorn918@gmail.com';
+$subject = '$q1 $q2 $q3 $4';
+$message = 'This mail is sent using the PHP mail function';
+$headers = 'From: you@you.com';
+mail($to_email,$subject,$message,$headers);
 ?>
