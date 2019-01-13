e7b724dbb5b57263d4dd83997a7575157951bd91
mikeh0rn <mikelaurencehorn@gmail.com>
2019-01-13 12:13:04 -0800

    trying something to avoid getting trapped in spam

--- send.php
+++ send.php
@@ -8,6 +8,13 @@ $q4=$_POST['q4'];
 $to_email = 'mhorn918@gmail.com';
 $subject = '$q1 $q2 $q3 $4';
 $message = 'This mail is sent using the PHP mail function';
-$headers = 'From: you@you.com';
+// $headers = 'From: you@you.com';
+
+$headers = array("From: mikelaurencehorn@gmail.com",
+    "Reply-To: mikelaurencehorn@gmail.com",
+    "X-Mailer: PHP/" . PHP_VERSION
+);
+$headers = implode("\r\n", $headers);
+
 mail($to_email,$subject,$message,$headers);
 ?>
