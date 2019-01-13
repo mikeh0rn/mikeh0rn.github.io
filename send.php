c34587278c354ee2a02aa101c463478e9ea9c146
mikeh0rn <mikelaurencehorn@gmail.com>
2019-01-13 11:59:02 -0800

    try again

--- send.php
+++ send.php
@@ -1,4 +1,4 @@
-<?php
+<?
 $q1=$_POST['q1'];
 $q2=$_POST['q2'];
 $q3=$_POST['q3'];
@@ -7,5 +7,5 @@ $to = "mhorn918@gmail.com";
 $subject = "Hi Mike $q3";
 $message= "$q1 $q2 :\n\n: $q1\n\n: $q2\n\nEmail: $q3\n\nTel: $q4\n";
 mail($to,$subject,$message);
-header("Location:index.html");
+header("Location:index.php");
 ?>
